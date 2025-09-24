b81100048ae35bef3d72dc20b505fff0a30fae16
ono-shitsuji <fbernard.kylian@gmail.com>
2025-09-24 09:54:14 +0200

    hope is the thing with feathers

---
+++ cours/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cours et exo en POO</title>
+</head>
+<body>
+    <a href="/cours/index.php">Cours</a>
+    <a href="http://">Exercices</a>
+</body>
+</html>
